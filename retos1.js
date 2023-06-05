ced7838bb3aa3a8a023e6419178d1b55ab87ed36
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-05 11:49:33 +0200

    dias 1,2,3

---
+++ retos1.js
@@ -0,0 +1,15 @@
+const print = () => {
+
+    console.log('Mensaje 1');
+
+    setTimeout(()=> {
+        console.log('Mensaje 2');
+        console.log('Mensaje 3');
+    }, 3000)
+
+}
+
+print();
+
+
+
