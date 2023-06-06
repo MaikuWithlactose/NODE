f006b3df77d86df93f245f288a3a2acee363ef3b
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-06 13:42:32 +0200

    promesas

--- retos23/retos2.js
+++ retos23/retos2.js
@@ -1,12 +1,17 @@
-const fs = require("fs");
+const fs = require('fs/promises');
 
 let user = { name: 'Jill', surname: 'Valentine', age: 35 };
 
-fs.writeFile('./objetouser.json', JSON.stringify(user), () => {
-    fs.readFile('./objetouser.json', 'utf-8', (err, info) => {
-        user = JSON.parse(info);
-        console.log('Nombre:', user.name);
-        console.log('Apellido:', user.surname);
-        console.log('Edad:', user.age);
-    });
-}) 
+fs.writeFile('./objetouser.json', JSON.stringify(user))
+  .then(() => {
+    return fs.readFile('./objetouser.json', 'utf-8');
+  })
+  .then(info => {
+    user = JSON.parse(info);
+    console.log('Nombre:', user.name);
+    console.log('Apellido:', user.surname);
+    console.log('Edad:', user.age);
+  })
+  .catch(error => {
+    console.error(error);
+  });
--- retos23/retos3.js
+++ retos23/retos3.js
@@ -1,28 +1,56 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 let user2 = {};
 
-rl.question('Como te llamas?:', (name) => {
-    user2.name = name;
-    rl.question('Como te apellidas?: ', (surname) => {
-        user2.surname = surname;
-        rl.question('Qué edad tienes?:', (age) => {
-            user2.age = age;
-            fs.writeFile('./objetouser2.json', JSON.stringify(user2), () => {
-                fs.readFile('objetouser2.json', 'utf-8', (err, info) => {
-                    user2 = JSON.parse(info);
-                    console.log('===================');
-                    console.log('Nombre:', user2.name);
-                    console.log('Apellido:', user2.surname);
-                    console.log('Edad:', user2.age);
-                    console.log('===================');
-                });
-                console.log("Escritura completada.")
-            });
-            rl.close();
-        });
-    }); 
-})
+const questionName = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('Como te llamas?: ', (name) => {
+      user2.name = name;
+      resolve();
+    });
+  });
+};
 
+const questionSurname = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('Como te apellidas?: ', (surname) => {
+      user2.surname = surname;
+      resolve();
+    });
+  });
+};
+
+const questionAge = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('Qué edad tienes?: ', (age) => {
+      user2.age = age;
+      resolve();
+    });
+  });
+};
+
+questionName()
+  .then(questionSurname)
+  .then(questionAge)
+  .then(() => {
+    return fs.writeFile('./objetouser2.json', JSON.stringify(user2));
+  })
+  .then(() => {
+    return fs.readFile('./objetouser2.json', 'utf-8');
+  })
+  .then(info => {
+    user2 = JSON.parse(info);
+    console.log('===================');
+    console.log('Nombre:', user2.name);
+    console.log('Apellido:', user2.surname);
+    console.log('Edad:', user2.age);
+    console.log('===================');
+    console.log('Escritura completada.');
+    rl.close();
+  })
+  .catch(error => {
+    console.error(error);
+    rl.close();
+  });
