bb720c646b64230adabaaeed2c51f7ceda9dcb38
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-06 22:43:04 +0200

    Correctivo

--- proyecto_node/writeAndReadObject.js
+++ proyecto_node/writeAndReadObject.js
@@ -5,7 +5,6 @@ function writeAndReadObject(objetouser, user, callback) {
     fs.writeFile(objetouser.json, JSON.stringify(user), () => {
         fs.readFile(objetouser, 'utf-8', (err, datos) => {
             callback(JSON.parse(datos));
-            
         });
     })
 }
