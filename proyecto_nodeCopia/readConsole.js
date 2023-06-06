9dc2eb0500dcad049719053bc5acaba7e4771607
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-06 23:08:41 +0200

    correctivo

--- proyecto_nodeCopia/readConsole.js
+++ proyecto_nodeCopia/readConsole.js
@@ -1,25 +1,50 @@
-const readline = require("readline");
-const rl = readline.createInterface(process.stdin, process.stdout);
+const fs = require('fs/promises');
+const readline = require('readline');
 
-function readConsole() {
+function Pregunta(TextoPregunta) {
   return new Promise((resolve, reject) => {
-    let user = {};
-
-    rl.question('Cual es tu nombre? ', (name) => {
-      user.name = name;
-
-      rl.question('Cual es tu apellido? ', (surname) => {
-        user.surname = surname;
-
-        rl.question('Cual es tu edad? ', (age) => {
-          user.age = parseInt(age);
-
-          rl.close();
-          resolve(user);
-        });
-      });
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
     });
+
+    rl.question(TextoPregunta,(respuesta) => {
+      resolve(respuesta);
+      rl.close();
+    })
   });
 }
+
+async function readConsole() {
+  try {
+    //Obtenemos los resultados
+    const nombre = await Pregunta("Cuál es tu nombre?")
+  
+    const apellido = await Pregunta("Cual es tu apellido?")
+  
+    const edad = await Pregunta("Cual es tu edad?")
+
+    //Generamos un objeto JSON
+    const user = { name: nombre, surname: apellido, age: edad }
+
+    //Lo cargamos en el fichero de memoria
+    const FicheroObjetivo = 'objetouser.json';
+    const Codificacion = 'utf-8';
+    const ContenidoFichero = JSON.stringify(user);
+
+    await fs.writeFile(FicheroObjetivo, ContenidoFichero, Codificacion)
+    .then( () => {
+      const LecturaFichero = fs.readFile( FicheroObjetivo, Codificacion)
+      .then(() => {
+        console.log("El usuario leído de "+FicheroObjetivo+" es "+ LecturaFichero)
+      })
+    });
+
+  } catch (error) {
+    console.log("Error: "+ error)
+  }
+}
+
+
 
 exports.readConsole = readConsole;
