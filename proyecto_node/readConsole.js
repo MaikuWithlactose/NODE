c405166d26214d99cd96d084affd18815b050384
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-06 22:42:53 +0200

    Correccion callbacks con .then

--- proyecto_node/readConsole.js
+++ proyecto_node/readConsole.js
@@ -4,20 +4,39 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 function readConsole(callback) {
     let user = {};
 
-    rl.question('Cual es tu nombre', (name) => {
-        user.name = name;
+    // rl.question('Cual es tu nombre', (name) => {
+    //     user.name = name;
 
-        rl.question('Cual es tu apellido:', (surname) => {
-            user.surname = surname;
+    //     rl.question('Cual es tu apellido:', (surname) => {
+    //         user.surname = surname;
 
-            rl.question('Cual es tu edad:', (age) => {
-                user.age = parseInt(age);
+    //         rl.question('Cual es tu edad:', (age) => {
+    //             user.age = parseInt(age);
                 
+    //             callback(user);
+    //             rl.close();
+    //         });
+    //     });
+    // });
+    rl.question("¿Cómo te llamas?")
+    .then((nombre) => {  
+        user.name = nombre;
+
+        return rl.question("¿Cuál es tu apellido?")
+        .then((apellido) => {
+            user.surname = apellido;
+
+            return rl.question("¿Cuántos años tienes?")
+            .then((edad) => {
+                user.age = edad
+
                 callback(user);
-                rl.close();
-            });
-        });
+            })
+
+        })
     });
+
+    rl.close();
 }
 
 exports.readConsole = readConsole;
