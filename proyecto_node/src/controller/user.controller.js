d6fd3acba170604bda69298f19f202c8ee5022d8
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-06-07 14:00:46 +0200

    subida

---
+++ proyecto_node/src/controller/user.controller.js
@@ -0,0 +1,25 @@
+let usuario = null; //(nombre: "Jose", apellidos: "Garcia Garcia"}
+
+function getStart(request, response)
+
+{
+let respuesta = {error: true, codigo: 200, mensaje: 'Punto de inicio'}; 
+response.send(respuesta);
+
+}
+
+
+function getUser(request, response)
+{
+    let respuesta;
+    if (usuario != null)
+
+        respuesta = {error: false, codigo: 200, data: usuario};
+
+    else
+
+     respuesta = {error: true, codigo: 200, mensaje: "El usuario no existe"};
+
+   response.send(respuesta); 
+
+}
